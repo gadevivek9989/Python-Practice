99f092d36c926f389254ece16c3d3da6a6ce55fa
gadevivek9989 <gadevivekreddy35@gmail.com>
2024-08-29 20:34:44 +0530

    func

---
+++ Basics/default_argument_harry.py
@@ -0,0 +1,6 @@
+def goodday(name, ending = 'thank you'):
+    print(f'good day, {name}')
+    print(ending)
+
+goodday('vivek')
+goodday('vikas' , 'thanks')  #if you not give defualt ending it will take default ending in argument
